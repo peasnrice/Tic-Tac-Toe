7e17d812e7f864103f0d8af4d1a863132effbfe6
Andy <andrew.j.c.e@gmail.com>
2024-10-31 11:07:55 -0400

    added basic game logic, can start on visuals

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -14,6 +14,7 @@ function createGame() {
 
     let player1 = null;
     let player2 = null;
+    let playersTurn = null;
 
     const board = []
     for (let i = 0; i < 3; i++) {
@@ -24,6 +25,7 @@ function createGame() {
         if (!player1) {
             player1 = player;
             player1.setMarker("X");
+            playersTurn = player1;
             return player1;
         }
         if (!player2) {
@@ -35,9 +37,28 @@ function createGame() {
             alert("Cannot add another player!, this is a two player game!");
         }
     }
-    const playPiece = (player, position) => {
-        if (board[position.x, position.y]) {
-
+    const playPiece = (player, positionX, positionY) => {
+        if (player === playersTurn) {
+            if (positionX > 2 || positionX < 0 || positionY > 2 || positionY < 0) {
+                alert("illegal move, placement out of bounds, choose a valid location");
+                return false;
+            }
+            if (!board[positionX][positionY]) {
+                board[positionX][positionY] = player.getMarker();
+                if (player === player1) {
+                    playersTurn = player2
+                } else {
+                    playersTurn = player1;
+                }
+                return true;
+            }
+            else {
+                alert("place already taken, please choose another!");
+                return false;
+            }
+        } else {
+            alert("wrong players turn!");
+            return false;
         }
     };
 
@@ -46,12 +67,20 @@ function createGame() {
 
     return { player1, player2, addPlayer, getBoard, playPiece, getPlayers, board };
 }
-
+// create players
 const player1 = createPlayer("Andy");
 const player2 = createPlayer("Safie");
+
+// create game
 const game = createGame();
+
+// add players to game
 game.addPlayer(player1);
 game.addPlayer(player2);
 
+game.playPiece(player1, 0, 0);
 console.log(game.getBoard());
-console.log(game.getPlayers());
+
+game.playPiece(player2, 2, 2);
+console.log(game.getBoard());
+
